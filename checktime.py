a0d40a91399967928a2d40d9ca750f732d328ad1
A1d3r <a1d3r13@gmail.com>
2025-04-19 13:02:20 -0400

    Cleaned workspace and applied .gitignore

--- checktime.py
+++
@@ -1,4 +0,0 @@
-﻿import pandas as pd
-df = pd.read_csv("data/mlb_boxscores_cleaned.csv")
-print(df["Game Date"].unique())
-
--- run_pipeline.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\a1d3r\source\repos\ScratchModelV2
-C:\Users\a1d3r\AppData\Local\Programs\Python\Python312\python.exe ScratchModelV2.py 
-
